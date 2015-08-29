476ac7378aeaf79ea5876439b749f31cd9005cee
nathanwblair <nathan.w.blair@gmail.com>
2015-08-29 23:02:50 +1000

    ZCommit

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour
+{
+	public GameObject[] items = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+		if (items != null)
+		{
+			for (int i = 0; i < items.Length; i++)
+			{
+				Instantiate(items[i], transform.GetChild(i).position, Quaternion.identity);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+}
