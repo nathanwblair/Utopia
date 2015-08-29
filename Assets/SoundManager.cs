d76140cd1ec6b5b40f0ab8353857c9286edcd4c8
nathanwblair <nathan.w.blair@gmail.com>
2015-08-30 04:48:49 +1000

    Added stuff

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class SoundManager : MonoBehaviour
 {
+	AudioSource soundClip = null;
+	AudioSource soundTrack;
 
+	int currentClipPriority = 0;
 
 	// Use this for initialization
 	void Start ()
@@ -16,4 +19,26 @@ public class SoundManager : MonoBehaviour
 	{
 
 	}
+
+	public void SetClip(AudioSource source, int priority)
+	{
+		if (soundClip != null)
+		{
+			if (priority < currentClipPriority)
+			{
+				soundClip.Stop();
+				soundClip = source;
+				soundClip.Play();
+				currentClipPriority = priority;
+			}
+		}
+		else
+		{
+			soundClip = source;
+			soundClip.Play();
+			currentClipPriority = priority;
+		}
+	}
+
+
 }
